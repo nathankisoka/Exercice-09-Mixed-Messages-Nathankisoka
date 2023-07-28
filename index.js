4fe9fa829396533291ecdfb5e90236748ba28129
nathan kisoka <nathankisoka1@gmail.com>
2023-07-28 15:38:24 +0100

    my index

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 //hello nathanael
+const information = {
+    name: ['PC', 'machine',,],
+    verb: ['are-you sleeping?', 'still not in bed?', 'drinking water?', 'whatsapp', 'or something like that?'],
+    adjective: ['good', 'better', 'pretty', 'fabulous']
+  }
